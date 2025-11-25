8c083a3bfe6bb70f00736fd683d81d814ac56e4d
The MD Traders <themdtraders@gmail.com>
2025-11-25 06:03:54 +0000

    i  otic when i save image of any thing like customer detail so in this a

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -48,7 +48,6 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&family=Roboto+Mono:wght@500;700&display=swap" rel="stylesheet" />
-        <link href="https://fonts.googleapis.com/css2?family=Noto+Nastaliq+Urdu:wght@400;500;600;700&display=swap" rel="stylesheet" />
       </head>
       <body className="antialiased">
         <ThemeProvider
